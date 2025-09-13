1b33dc33350aede0966e89b5cefaecfd492a4fd5
Abdulrahman_Saad <abdulrahman.saad2303@gmail.com>
2025-09-14 02:05:07 +0300

    try #2 to fix netlify bug with route

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ export default function Loader({ width, borderWidth }) {
   return (
     <div className="flex justify-center items-center">
       <div
-        className={`w-${width || "6"} h-${width || "6"} border-${
+        className={`w-${width || 6} h-${width || 6} border-${
           borderWidth || 2
         } border-gray-300 border-t-emerald-600 rounded-full animate-spin`}
       ></div>
