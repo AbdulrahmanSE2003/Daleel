045c8e57af6cb742d6c2134bde17babff994b0cb
Abdulrahman_Saad <abdulrahman.saad2303@gmail.com>
2025-09-14 02:10:41 +0300

    modified some style in linkCard

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -2,13 +2,13 @@ import { Edit, Trash2 } from "lucide-react";
 
 function LinkCard({ link, onDelete, onEdit }) {
   return (
-    <div className="group relative flex flex-col md:flex-row w-full items-start md:items-center justify-between bg-white/95 backdrop-blur-sm shadow-lg rounded-2xl p-6 py-3  hover:shadow-xl  transition-all duration-300 ease-in-out animate-glow-in overflow-hidden border-2 border-transparent hover:border-emerald-600 ">
+    <div className="group relative flex flex-col md:flex-row w-full items-start md:items-center justify-between bg-white/95 backdrop-blur-sm shadow-lg rounded-2xl p-6 py-3  hover:shadow-xl  transition-all duration-500 ease-in-out animate-glow-in overflow-hidden border-2 border-transparent hover:border-emerald-500 ">
       <div className="flex flex-col gap-4 w-full relative z-10">
         <div className="flex items-center gap-4">
           <span className="text-3xl transform group-hover:scale-110 group-hover:rotate-6 transition-transform duration-400">
             {link.emoji}
           </span>
-          <p className="font-sans font-semibold text-gray-900 text-xl md:text-2xl tracking-tight group-hover:text-emerald-700 transition-colors duration-300">
+          <p className="font-sans font-semibold text-gray-900 text-xl md:text-2xl tracking-tight  transition-colors duration-500">
             {link.title}
           </p>
         </div>
@@ -18,10 +18,10 @@ function LinkCard({ link, onDelete, onEdit }) {
             {link.tags.map((tag, idx) => (
               <span
                 key={idx}
-                className="relative text-xs bg-[#0c5563]/5 text-emerald-800 px-3 py-1.5 rounded-full font-medium border border-[#0c8f63]/30 hover:bg-[#0c8f63]/20 hover:scale-105 transition-all duration-300 group/tag"
+                className="relative text-xs bg-[#0c5563]/5 text-emerald-800 px-3 py-1.5 rounded-full font-medium border border-[#0c8f63]/30 hover:bg-[#0c8f63]/15 hover:scale-105 transition-all duration-300 group/tag"
               >
                 #{typeof tag === "string" ? tag : tag.name}
-                <div className="absolute inset-0 bg-[#0c8f63]/20 rounded-full blur-sm group-hover/tag:blur-md transition-all duration-300"></div>
+                <div className="absolute inset-0 bg-[#0c8f63]/10 rounded-full group-hover/tag:blur-xs transition-all duration-300"></div>
               </span>
             ))}
           </div>
