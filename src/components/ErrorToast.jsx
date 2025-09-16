6678251a070e52d5b33ca1fef5fc7d8e70c5af6c
Abdulrahman_Saad <abdulrahman.saad2303@gmail.com>
2025-09-16 12:21:18 +0300

    added validate on url ip before sending

--- src/components/ErrorToast.jsx
+++ src/components/ErrorToast.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function ErrorToast({ message, onClose }) {
+function ErrorToast({ message, onClose, className }) {
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
@@ -26,9 +26,9 @@ function ErrorToast({ message, onClose }) {
 
   return (
     <div
-      className={`fixed top-5 right-5 z-50 capitalize text-md bg-red-200 border border-red-500 text-black/80 px-6 py-3 rounded-lg shadow-lg transition- duration-500 -tracking-tight ${
+      className={`fixed top-5 right-5 z-[100] capitalize text-md bg-red-200 border border-red-500 text-black/80 px-6 py-3 rounded-lg shadow-lg transition- duration-500 -tracking-tight ${
         visible ? "translate-x-0 opacity-100" : "translate-x-24 opacity-0"
-      }`}
+      } ${className && className}`}
     >
       {message}
     </div>
