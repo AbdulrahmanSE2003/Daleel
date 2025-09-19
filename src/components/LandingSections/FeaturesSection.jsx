71f54886cc88767cafd8cb5164094ad6898997e6
Abdulrahman_Saad <abdulrahman.saad2303@gmail.com>
2025-09-19 12:07:27 +0300

    fix: footer bug i18n

--- src/components/LandingSections/FeaturesSection.jsx
+++ src/components/LandingSections/FeaturesSection.jsx
@@ -26,7 +26,7 @@ export default function FeaturesSection() {
 
   return (
       <section className="py-16 bg-gray-50">
-        <div className="max-w-6xl mx-auto px-16 sm:px-36 md:px-14 lg:px-6">
+        <div className="max-w-6xl mx-auto px-10 sm:px-36 md:px-14 lg:px-6">
           <h2 className="text-3xl font-bold text-center mb-12 ">
             {t("features.heading")}{" "}
             <span className="text-emerald-500">{lang === "en" ? "دليل" : "Daleel"}</span>{lang === "en" ? "؟" : "?"}
@@ -38,7 +38,7 @@ export default function FeaturesSection() {
                   <div className="absolute inset-0 rounded-2xl bg-[length:300%_300%] transition-all duration-500 group-hover:scale-105"></div>
 
                   {/* Card Content */}
-                  <div className="relative h-60 bg-white border border-gray-300 rounded-2xl p-6 text-center duration-500 group-hover:scale-105 group-hover:shadow-xl transition shadow-emerald-200">
+                  <div className="relative h-60 overflow-hidden bg-white border border-gray-300 rounded-2xl p-6 text-center duration-500 group-hover:scale-105 group-hover:shadow-xl transition shadow-emerald-200">
                     <div className="flex justify-center mb-4">{feature.icon}</div>
                     <h3 className="text-xl font-semibold mb-2">{feature.title}</h3>
                     <p className="text-gray-600">{feature.description}</p>
